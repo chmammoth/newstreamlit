7e1c0684de3bbea255a3e86ca2e1c435ef602c8a
chmammoth <164169517+chmammoth@users.noreply.github.com>
2024-03-20 18:24:47 +0700

    Add files via upload

--- apppp.py
+++ apppp.py
@@ -1,4 +1,21 @@
 import streamlit as st
+import qrcode
+
+
+def creat_qrcode(tex):
+    qr = qrcode.QRCode(
+    version=1,
+    error_correction=qrcode.constants.ERROR_CORRECT_L,
+    box_size=10,
+    border=4,
+    )
+    qr.add_data(tex)
+    qr.make(fit=True)
+
+    img = qr.make_image(fill_color="black", back_color="white")
+    img.save("qrcode.png")
+
+
 
 
 
@@ -6,5 +23,6 @@ text = st.text_area("enter text")
 submitbtn = st.button("Enter")
 
 if submitbtn:
-    st.balloons()
+    creat_qrcode(text)
+    st.image("qrcode.png")
     st.success(text)
